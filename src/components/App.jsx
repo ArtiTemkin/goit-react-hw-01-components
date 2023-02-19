e81a3c3de48ad533d39e38c28eacb740004571c7
ArtiTemkin <a.zymarev@icloud.com>
2023-02-19 21:49:11 +0200

    ошибка сборки

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import { Profile } from './User/Profile';
+import user from './User/user.json'
+
 export const App = () => {
   return (
     <div
@@ -10,6 +13,9 @@ export const App = () => {
         color: '#010101'
       }}
     >
+      <div>
+        <Profile user={user} />
+      </div>
       React homework template
     </div>
   );
